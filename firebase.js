e9e105d717527986dddeae43fcc46ca984868429
kleberson154 <kleberson55@hotmail.com>
2022-04-16 18:57:14 -0300

    add .env

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCXIX-9Pg5W7O4QRQyrxCOxeTu7aoDMa2g',
-  authDomain: 'clone-whatsapp-53f19.firebaseapp.com',
-  projectId: 'clone-whatsapp-53f19',
-  storageBucket: 'clone-whatsapp-53f19.appspot.com',
-  messagingSenderId: '275898406062',
-  appId: '1:275898406062:web:cf534b5ca35535baf9893f'
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
